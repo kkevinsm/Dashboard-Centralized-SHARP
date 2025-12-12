a7a02f00dfd19eda42a3e1f1e57c5c7ba26b0841
Kkevinsm <karashii866@gmail.com>
2025-12-12 16:40:58 +0700

    change: tittle

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69879351074a96b46fc770ef05449180a2f918ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f2b21e1b86551cc594e5f4e59b02f834a55577")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
