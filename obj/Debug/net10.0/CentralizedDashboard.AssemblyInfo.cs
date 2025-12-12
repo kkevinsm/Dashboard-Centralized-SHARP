69879351074a96b46fc770ef05449180a2f918ef
Kkevinsm <karashii866@gmail.com>
2025-12-12 16:09:07 +0700

    fix: sidebar-good

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dbdca08a798d0d235010f179cff73fd4b38bc07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ad38395c910f01b8a7b62dfd5ec6815878a2cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
