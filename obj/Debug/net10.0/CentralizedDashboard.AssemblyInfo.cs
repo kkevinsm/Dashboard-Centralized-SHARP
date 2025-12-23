b4d252e30ab5e732338b3278bc0d796f0e7619ad
Kkevinsm <karashii866@gmail.com>
2025-12-23 16:06:36 +0700

    feat: add days-badge-header

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf499517096c6643ad11a95270aa24eba5ff82e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e9ff21b27feaa5f3f3d44180a9302721e44056")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
