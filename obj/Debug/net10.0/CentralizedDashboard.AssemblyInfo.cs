ca783e1b781ccff6ee011639838a7300f8ba46d4
unknown <reynal.viverio@gmail.com>
2025-12-24 15:11:43 +0700

    Coba

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532d971f1160f724f62ba0f2e1a276f6167ed816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a367f110ea7de88c0009446eaf60ae419720ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
