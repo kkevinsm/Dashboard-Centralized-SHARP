2778bdad2386f8d88030f9eb7f076e78e9ca5be5
Kkevinsm <karashii866@gmail.com>
2026-01-06 16:48:34 +0700

    feat: indicator machine/process

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a404d812eca2dfa776134f22bd24cace25d06def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97dcbbfd5b3de5adb763023745ab53007e64036a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
