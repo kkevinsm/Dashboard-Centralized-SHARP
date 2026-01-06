a404d812eca2dfa776134f22bd24cace25d06def
Kkevinsm <karashii866@gmail.com>
2026-01-06 10:30:00 +0700

    feat: new dashboard-3d

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7135a6b4d52ca08606ad25c7550641cfa60af53c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c94ab2d205cf22f3f92f2cd9750bb30f8c95a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
