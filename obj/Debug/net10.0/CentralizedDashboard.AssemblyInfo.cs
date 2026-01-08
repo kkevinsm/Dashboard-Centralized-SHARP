d36624d77b58770d8aa07617048c64b4789f1bb8
seidwmpe <seidwmpe@gmail.com>
2026-01-08 13:26:36 +0700

    fixed: indicator position10

--- obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
+++ obj/Debug/net10.0/CentralizedDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5e71370142e9b98c1d9001afe57ddabfcc7268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb56b1fca32ed22f568e3086c96b4422c1171dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentralizedDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
